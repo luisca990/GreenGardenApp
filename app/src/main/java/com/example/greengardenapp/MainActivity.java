a0917d79d0abcd307fccfa0ad850f51a502b957e
Jorge Alejandro Achury Fonseca <jorge.achury@grupooet.com>
2023-10-26 19:57:31 -0500

    Login y Registro

--- app/src/main/java/com/example/greengardenapp/MainActivity.java
+++ app/src/main/java/com/example/greengardenapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.greengardenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
